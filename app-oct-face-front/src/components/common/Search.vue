8ceae380ef4951905b5ade46631d96d469a0dfeb
pakuyuya <pakuyuya@yahoo.co.jp>
2018-08-15 22:23:38 +0900

    remove suggest

--- app-oct-face-front/src/components/common/Search.vue
+++ app-oct-face-front/src/components/common/Search.vue
@@ -1,30 +1,19 @@
 <template>
   <div>
-    <vue-simple-suggest
-      class="search-form"
-      placeholder="Search repository or user..."
-      v-model="chosen"
-      :list="list"
-      :styles="autoCompleteStyle"
-      :destyled=true
-      :filter-by-query="true">
-      <div slot="suggestion-item" slot-scope="{ suggestion, query }">
-        <div><router-link :to="suggestion">{{ suggestion }}</router-link></div>
-      </div>
-    </vue-simple-suggest>
+    <input type="search" v-model="text" class="search-form" placeholder="Search repository or user...">
   </div>
 </template>
 
 <script>
-import VueSimpleSuggest from 'vue-simple-suggest'
-import apiSV from '@/service/apiSV'
-
 export default {
   name: 'Search',
-  components: {VueSimpleSuggest},
+  components: {},
   data: function () {
     return {
-      list: ['a', 'b'],
+      list: [
+        {title: 'user1', to: '/user1/'},
+        {title: 'user1repo1', to: '/user1/repo1/source'}
+      ],
       chosen: '',
       autoCompleteStyle: {
         inputWrapper: 'search-wrapper',
@@ -45,25 +34,5 @@ export default {
 .search-wrapper input {
   padding: 5px 8px;
   width: 450px;
-}
-.suggestions {
-  /* It's improtant to have a cpecific pivot point for transition:*/
-  opacity: 1;
-  background-color: #fff;
-  width: 470px;
-  position: absolute;
-}
-
-.z-1000 {
-  z-index: 1000;
-}
-
-.search-item {
-  padding: 5px 8px;
-  margin: 3px 0px;
-}
-.search-item.hover {
-  background: #ddd;
-  cursor: pointer;
 }
 </style>
