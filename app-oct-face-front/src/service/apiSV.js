fa874df2473e1822bda773d971a3362163bce898
pakuyuya <pakuyuya@yahoo.co.jp>
2018-08-30 23:35:53 +0900

    fix doc comment

--- app-oct-face-front/src/service/apiSV.js
+++ app-oct-face-front/src/service/apiSV.js
@@ -8,11 +8,11 @@ import httputil from '@/jscode/httputil'
 exports.default = {
   /**
    * get is function call RestAPI on server with GET request.
-   * @param baseurl {string} url. If you specify a URL parameter like `/url/:id`, we will replace it with the contents of params
+   * @param urlpath {string} part of url before API server url in config. If you specify a URL parameter like `/url/:id`, we will replace it with the contents of params
    * @param params {string} query parameter.
    */
-  get: function (baseurl, params) {
-    let apiurl = config.apiserver + (baseurl.startsWith('/') ? '' : '/') + baseurl
+  get: function (urlpath, params) {
+    let apiurl = config.apiserver + (urlpath.startsWith('/') ? '' : '/') + urlpath
     let query = {};
     for (let v in params) {
       let prevurl = url
